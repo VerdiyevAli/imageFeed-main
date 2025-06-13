f0b0b344fe34dc61b7c0a75edcc916c0ce63acf3
VerdiyevAli <verdiyevios8@gmail.com>
2025-06-13 12:37:38 +0400

    Начальный коммит проекта

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+
+enum Constants {
+    enum API {
+        static let accessKey = "S-5kWb2JOLUERhUrddgO58BjdEnONLIyd4AdprXnc5U"
+        static let secretKey = "PARV2GUTX3j9pZAuNqFJqxmIWsf0ezMF9lyRAhqGyrI"
+        static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+        static let accessScope = "public+read_user+write_likes"
+        static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    }
+}
+
+
+//
+//
+//
